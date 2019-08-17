49e03258a92d5388316aa5b0e124d1add4a4f1bf
cjanarthanangit <janaece_12@rocketmail.com>
2019-08-17 16:41:23 +0530

    Changed for UTF-8 Coding.

--- tes.py
+++ tes.py
@@ -1,11 +1,14 @@
 import json
 
-a_dict = {'new_key': 'new_value'}
+a_dict = {'new_key1': 'new_value2'}
 
 with open('rt.json') as f:
     data = json.load(f)
 
 data.update(a_dict)
 
-with open('test.json', 'w') as f:
-    json.dump(data, f)
+##with open('test.json', 'w') as f:
+##    json.dump(data, f)
+
+with open('test.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
